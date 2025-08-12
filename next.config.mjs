124c859c7ac7c99892c5f83dd143dc94d394f7cd
Afolabi Peter <afolabipeter49@gmail.com>
2025-08-12 07:21:53 +0100

    Work Update

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cloud.appwrite.io',
+        hostname: 'nyc.cloud.appwrite.io',
         pathname: '**',
       },
     ],
